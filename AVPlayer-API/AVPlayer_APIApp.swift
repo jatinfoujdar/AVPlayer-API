935f2fc880792d34c539c6068fdf5ff2b67a00d8
Jatin Foujdar <jatin321001@gmail.com>
2024-09-04 21:57:40 +0530

    Initial Commit

---
+++ AVPlayer-API/AVPlayer_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVPlayer_APIApp.swift
+//  AVPlayer-API
+//
+//  Created by jatin foujdar on 04/09/24.
+//
+
+import SwiftUI
+
+@main
+struct AVPlayer_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
